12f701701438a693ac8ed0ed3713e55fcb68dc2b
Leo Stelon <53221136+leostelon@users.noreply.github.com>
2023-09-26 15:00:46 +0530

    download bug

--- src/routes/saturn.js
+++ src/routes/saturn.js
@@ -6,19 +6,29 @@ const { download } = require("../utils/download");
 router.get("/:cid", async (req, res) => {
 	try {
 		// Download from Saturn
-		let dest = req.params.cid + ".car";
-		await download(`https://saturn.ms/ipfs/${req.params.cid}`, dest);
+		const uniqueStamp = Date.now();
+		let dest = req.params.cid + `-${uniqueStamp}.car`;
+		const result = await download(
+			`https://saturn.ms/ipfs/${req.params.cid}`,
+			dest
+		);
+		if (!result) return res.status(500).send({ message: "Unknown error!" });
 		// Unpack and zip the file
 		let zipPath = await unpackAndArchive(dest);
 		// Delete the downloaded car file
-		fs.unlinkSync(dest, { recursive: true, force: true });
+		fs.rmSync(dest, { recursive: true, force: true });
 		// Send it to client
 		var fileReadStream = fs.createReadStream(zipPath);
 		res.setHeader("Content-disposition", "attachment; filename=" + zipPath);
-		fileReadStream.pipe(res).on("finish", () => {
-			// Remove the zipped file
-			fs.rmSync(zipPath, { recursive: true, force: true });
-		});
+		fileReadStream
+			.pipe(res)
+			.on("finish", () => {
+				// Remove the zipped file
+				fs.rmSync(zipPath, { recursive: true, force: true });
+			})
+			.on("error", () => {
+				console.log("errored");
+			});
 	} catch (error) {
 		res.status(500).send({ message: error.message });
 	}
--- src/utils/download.js
+++ src/utils/download.js
@@ -3,22 +3,32 @@ const https = require("https");
 var fs = require("fs");
 
 var download = function (url, dest) {
-	return new Promise((res, rej) => {
-		var file = fs.createWriteStream(dest);
-		https
-			.get(url, function (response) {
-				response.pipe(file);
-				file.on("finish", function () {
-					file.close(); // close() is async, call cb after close completes.
-					res(true);
+	try {
+		return new Promise((res, rej) => {
+			var file = fs.createWriteStream(dest);
+			https
+				.get(url, function (response) {
+					response.pipe(file);
+					file
+						.on("finish", function () {
+							file.close(); // close() is async, call cb after close completes.
+							res(true);
+						})
+						.on("error", (_) => {
+							console.log("errored");
+						});
+				})
+				.on("error", function (err) {
+					// Handle errors
+					fs.rmSync(dest, { recursive: true, force: true });
+					rej(err.message);
 				});
-			})
-			.on("error", function (err) {
-				// Handle errors
-				fs.unlink(dest); // Delete the file async. (But we don't check the result)
-				rej(err.message);
-			});
-	});
+		});
+	} catch (err) {
+		console.log(err.message);
+		fs.rmSync(dest, { recursive: true, force: true });
+		rej(false);
+	}
 };
 
 module.exports = { download };
