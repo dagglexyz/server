5ad325a1bce6cb2e1a8ad40045945c119d700c4f
Leo Stelon <53221136+leostelon@users.noreply.github.com>
2023-09-30 18:59:04 +0530

    removed unwanted comments

--- src/utils/car.js
+++ src/utils/car.js
@@ -1,7 +1,6 @@
 async function unpackAndArchive(carFilePath) {
 	try {
 		const car = await import("@daggle/car");
-		// TODO: Download file from saturn and delete once archived!
 		const zipPath = await car.unpackAndArchive(carFilePath);
 
 		return zipPath;
