d03bda02f1626a92a0ddd0702df2133e9033fc21
leostelon <53221136+leostelon@users.noreply.github.com>
2023-12-09 20:17:22 +0530

    added jwt

--- src/routes/user.js
+++ src/routes/user.js
@@ -7,6 +7,7 @@ const {
 	magicLogin,
 } = require("../controllers/user");
 const auth = require("../middlewares/auth");
+const jwt = require("jsonwebtoken");
 
 const router = require("express").Router();
 
@@ -21,5 +22,26 @@ router.post("/signup", signUp);
 router.post("/login", login);
 
 router.post("/magicLogin", magicLogin);
+
+router.post("/jwt", async (req, res) => {
+	try {
+		if (!req.body.tokens) return res.status(500).send({ message: "Please send tokens list" });
+
+		let data = {}
+
+		req.body.tokens.forEach(element => {
+			const decoded = jwt.verify(element, "secret");
+			if (!data[decoded.address]) data[decoded.address] = 0;
+			data[decoded.address] += parseFloat(decoded.amount);
+		});
+		let address = Object.keys(data);
+		let amount = Object.values(data);
+
+		return res.send({ address, amount })
+	} catch (error) {
+		res.status(500).send({ message: error.message });
+	}
+});
+
 
 module.exports = router;
